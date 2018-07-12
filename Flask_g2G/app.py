e89b7ac15879f9de8ad03868a25528a4f48d1899
jhemin0415 <jhemin0415@naver.com>
2018-07-12 17:10:19 +0900

    소켓 삭제

--- Flask_g2G/app.py
+++ Flask_g2G/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, session
-from flask_socketio import SocketIO, emit, join_room
+
 
 class Userinfo:
     def __init__(self, id, nickname, permission):
