40538f6a72a2c55889cb7a31bd671da4b77420e4
jhemin0415 <jhemin0415@naver.com>
2018-07-12 15:34:58 +0900

    Revert "test commit"
    This reverts commit 55ad041e8cbcb3bfc61735ba1487bc155f800952.

--- Flask_g2G/app.py
+++ Flask_g2G/app.py
@@ -11,9 +11,6 @@ class Userinfo:
 
 
 
-
-
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -28,7 +25,7 @@ def post_login():
 
 @app.route('/<category>/post')
 def category_post():
-
+    
 
 if __name__ == '__main__':
     app.run(host = '203.252.231.149', port = 5000, debug = True)
